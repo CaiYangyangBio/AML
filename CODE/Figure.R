eadbc95a6b0f42bb54ffbdd47a04018c6bdb58ae
CaiYangyangBio <160556609+CaiYangyangBio@users.noreply.github.com>
2024-02-20 12:52:38 +0800

    Update Figure.R

--- CODE/Figure.R
+++ CODE/Figure.R
@@ -77,3 +77,46 @@ setwd("~/Desktop/22白血病/Runtime/2特征比较/")
 pdf("1D.pdf",width = 6,height = 5)
 g1
 dev.off()
+#==============================#
+
+#==========Figure 1F==========#
+setwd("~/Desktop/22白血病/Runtime/0rawdata/1-ncORF/0-fasta")
+canonical<-read.table("uniprot.fa",header = F,sep = "\t",check.names = F,stringsAsFactors = F,fill = T,quote = "")
+n<-nrow(canonical)
+len<-nchar(canonical[,1])[seq(2,n,2)]
+canonical<-data.frame(ORF = canonical[seq(1,n,2),1],len)
+canonical$ORF<-gsub(">","",canonical$ORF)
+
+setwd("~/Desktop/22白血病/Runtime/0rawdata/1-ncORF/1-SeqName/")
+non_canonical<-read.table("5nuORFRename.txt",header = T,sep = "\t",check.names = F,stringsAsFactors = F,fill = T,quote = "")
+non_canonical<-data.frame(ORF=non_canonical$ORF_ID_Rename,len=nchar(non_canonical$AA_seq))
+
+ORF<-rbind(canonical,non_canonical)
+
+setwd("~/Desktop/22白血病/Runtime/1预处理/")
+data<-read.table("All_info.txt",header = T,sep = "\t",stringsAsFactors = F,check.names = F,quote = "",fill = T)
+
+setwd("/Users/caiyangyang/Desktop/22白血病/Runtime/2特征比较/2.等位分组条形图/")
+MAP<-read.table("MAP_Allele_support.txt",header = T,sep = "\t",stringsAsFactors = F,check.names = F,quote = "",fill = T)
+
+index<-match(MAP$Epitopes,data$Epitopes,nomatch = 0)
+MAP$ORF<-data$`Leading razor protein`[index]
+
+index<-match(MAP$ORF,ORF$ORF,nomatch = 0)
+MAP$source_protein_len<-ORF$len[index]
+source<-MAP[,c("Epitopes","Type","source_protein_len")]
+source<-source[!duplicated(source),]
+
+g4<-gghistogram(source,x="source_protein_len",y="..density..",fill = "Type",rug = T,
+                color = "Type",palette = c("Canonical"="#4DBBD5FF","Non-Canonical"="#E64B35FF"),
+                ylab = "Density",xlab = "Source ORF Length(aa)")+
+  scale_x_continuous(limits = c(0,1000),breaks = seq(0,1000,100))
+
+setwd("~/Desktop/22白血病/Runtime/2特征比较")
+pdf("1F.pdf",width = 6,height = 5)
+g4
+dev.off()
+#==============================#
+
+
+
